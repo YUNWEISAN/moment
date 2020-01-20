2aa55ebd96a51d564c03db2edd31fbb0f5968103
yangyinghui <516512986@qq.com>
2020-01-20 11:36:19 +0800

    初始页面--代码更改

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,9 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
+<!--    <div id="nav">-->
+<!--      <router-link to="/">Home</router-link> |-->
+<!--      <router-link to="/about">About</router-link>-->
+<!--    </div>-->
     <router-view />
   </div>
 </template>
@@ -11,9 +11,9 @@
 <style lang="stylus">
 #app
   font-family 'Avenir', Helvetica, Arial, sans-serif
-  -webkit-font-smoothing antialiased
-  -moz-osx-font-smoothing grayscale
-  text-align center
-  color #2c3e50
-  margin-top 60px
+  /*-webkit-font-smoothing antialiased*/
+  /*-moz-osx-font-smoothing grayscale*/
+  /*text-align center*/
+  /*color #2c3e50*/
+  /*margin-top 60px*/
 </style>
